62875026fb7ad50ac523bb114c4ea4b49d3ae30d
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-09-24 23:46:17 -0700

    added transition

--- app/src/main/java/com/hanseltritama/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/motionlayoutdemo/MainActivity.kt
@@ -2,10 +2,34 @@ package com.hanseltritama.motionlayoutdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        my_motion_layout.setTransitionListener(object: MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+                motionLayout
+            }
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+                Log.d("HANSELTEST", "Progress:" + progress)
+                if (progress > 0f) {
+                    imageView.setImageResource(R.drawable.ic_oval_orange)
+                } else {
+                    imageView.setImageResource(R.drawable.ic_oval_grey)
+                }
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+            }
+        })
     }
 }
